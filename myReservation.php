9da44422f4b15e6423ca3b0cfefc40765d37ff84
ShanmukhaSrinivas <vemulasrinivas2505@gmail.com>
2020-01-18 14:35:26 +0530

    thirdCommit

---
+++ myReservation.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>myReservation</title>
+    <link rel="stylesheet" href="cssfile.css">
+</head>
+
+<body style="background-image: url(flight.jpg);">
+    <h4 style="padding-left: 40%;">Your booking at aqua is confirmed at</h4>
+    <h3 style="padding-left: 41%;">8PM--AQUA--18-JAN-2020</h3>
+    <h4 style="padding-left: 43%;">The code of your circle is</h4>
+    
+    <script>function myFunc() {
+            location.href = "option.html"
+        }</script>
+</body>
+
+</html>
